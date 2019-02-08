bb84f25c7689527d2eabf1b4413e42d734bf4926
melat tesfay <melat.tesfay@amethodschools.org>
2019-02-08 02:14:01 +0000

    mmm

--- js/script.js
+++ js/script.js
@@ -19,11 +19,13 @@ var links= ["https://www.youtube.com/watch?v=ApXoWvfEYVU","https://www.youtube.c
 "https://www.youtube.com/watch?v=6ONRf7h3Mdk","https://www.youtube.com/watch?v=viimfQi_pUw"];
 
     // BELOW Use forEach Loop to display the data from each of your array's in the correct div
+
 songs.forEach(function(element){
     $("#songs").append("<p>" + element + "</p>");
 });
 links.forEach(function(element){
-    $("#links").append("<a href=" + element + "> youtube </a>");
+    var mywidth = 200
+    $("#links").append("<iframe width='203' height='123' src='" + element + "'></iframe>");
    
 });
     
@@ -35,7 +37,9 @@ images.forEach(function(element){
     $("#images").append("<img src=" + element + ">");
 })
 
-
+song_lengths.forEach(function(element){
+    $("#lengths").append("<p>" + element + "</p>");
+});
 
 
 
@@ -60,9 +64,59 @@ function addSongInfo(){
 }
 
 $("#add").click(function() {
-    emptySongInfo();
-    addSongInfo();
-    displaySongInfo();
+    var song = $("#song").val();
+   
+   console.log(song);
+    
+    songs.push(song)
+    $("#songs").text("");
+
+ 
+ songs.forEach(function(songItem){
+    $("#songs").append("<p>" + songItem + "</p>");
+    });
+    
+     
+     var image = $("#image").val();
+   
+   console.log(image);
+    
+    images.push(image)
+    $("#images").text("");
+
+ 
+ images.forEach(function(imageItem){
+    $("#images").append("<img src=" + imageItem + ">");
+    });
+
+
+
+var artist = $("#artist").val();
+   
+   console.log(artist);
+    
+    artists.push(artist)
+    $("#artists").text("");
+
+ 
+ artists.forEach(function(artistItem){
+    $("#artists").append("<p>" + artistItem + "</p>");
+    });
+    
+    
+    var link = $("#link").val();
+   
+   console.log(link);
+    
+    links.push(link)
+    $("#links").text("");
+
+ 
+ links.forEach(function(linksItem){
+    $("#links").append("<a href=" + linksItem + "> youtube </a>");
+    });
+    
 });
 
-displaySongInfo();
+
+
